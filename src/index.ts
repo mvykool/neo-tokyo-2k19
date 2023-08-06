6c97ba5c4f27230be9adb5d9f7d76688f9be6561
mvykool <maykolandres4@gmail.com>
2023-08-06 18:24:09 -0500

    added dependencies and more

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,26 @@ import cheerio from "cheerio";
 
 const app = express();
 
+const mangaList: any[] = [];
+
+app.get('/', (req, res) => {
+  axios.get('https://inmanga.com')
+  .then((response) => {
+      const html = response.data
+      const $ = cheerio.load(html)
+
+      $('div:contains("panel")',html).each(function () {
+      const title = $(this).text()
+      mangaList.push({
+        title
+      })
+      })
+      res.json(mangaList)
+    })
+  .catch((err) => console.log(err))
+})
+
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   
