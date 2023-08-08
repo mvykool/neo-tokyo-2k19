50d4f4a4e7bc8f0993f0d34210e63d6a53185a21
mvykool <maykolandres4@gmail.com>
2023-08-07 19:36:37 -0500

    succesfully retrieved data

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,20 @@ import cheerio from "cheerio";
 
 const app = express();
 
+const mangaList: any[] = []
+
 app.get('/', (req, res) => {
     axios.get("https://inmanga.com")
     .then((response): any => {
       const html = response.data
-      res.json(html)
-    })
-});
+      const $ = cheerio.load(html)
+      $(".panel", html).each(function() {
+        const img = $(".ImageContainer img-responsive").attr("src")
+        console.log(img);
+        
+      })
+      })
+    });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
