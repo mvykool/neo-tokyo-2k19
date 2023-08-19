045ce4b0354dd015dfe6f2c5238e45d95d4e29dd
mvykool <maykolandres4@gmail.com>
2023-08-19 16:20:48 -0500

    connected to json

--- client/src/app/home/home/home.component.ts
+++ client/src/app/home/home/home.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+      jsonData: any = [];
 
+      constructor(private http: HttpClient) { }
+
+      ngOnInit(){
+        this.http.get('assets/db.json').subscribe((response: any) => {
+          console.log(response)
+          this.jsonData = response;
+        });
+      }
 }
