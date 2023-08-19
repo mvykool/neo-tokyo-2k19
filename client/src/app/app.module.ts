64df31b881d771c6f52702c6019c8ab0402a39de
maicol hernandez <maykolandres4@gmail.com>
2023-08-19 14:43:01 -0500

    setted up angular with a home component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
