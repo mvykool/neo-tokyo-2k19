73fd9171a6aec9e8c78c528e098592ed45a135cb
maicol hernandez <maykolandres4@gmail.com>
2023-08-24 20:34:24 -0500

    deleted the navbar as it wont be needed

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [NavbarComponent, FooterComponent, SidebarComponent],
+  declarations: [FooterComponent, SidebarComponent],
   imports: [
     CommonModule
   ],
-  exports: [NavbarComponent, FooterComponent, SidebarComponent]
+  exports: [ FooterComponent, SidebarComponent]
 })
 export class SharedModule { }
