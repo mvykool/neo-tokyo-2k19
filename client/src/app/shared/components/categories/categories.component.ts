c683a33f61e0e44525f08a49440ec04ab47d110f
maicol hernandez <maykolandres4@gmail.com>
2023-09-03 15:58:47 -0500

    final changes

--- client/src/app/shared/components/categories/categories.component.ts
+++ client/src/app/shared/components/categories/categories.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.scss']
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit {
+  defaultImage: string = "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg";
+  jsonData: any = [];
+  animeData: { thumbnail: string }[] = [];
 
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get('assets/db.json').subscribe((response: any) => {
+      this.jsonData = response.data.filter((item: any) => {
+        const tags = item.tags || []; 
+        return tags.includes('action') || tags.includes('adventure');
+      }).slice(100, 102);
+
+      this.animeData = this.jsonData.map((item: any) => ({
+        title: item.title,
+        thumbnail: item.picture
+      }));
+
+    });
+  }
+
+  handleImageError(item: any) {
+    item.thumbnail = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png";
+  }
 }
